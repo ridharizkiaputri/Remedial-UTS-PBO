72bc7ea20d3b678b3db3dac40d6fc3968f7c385c
ridharizkiaputri <48399924+ridharizkiaputri@users.noreply.github.com>
2019-04-27 17:14:38 +0800

    Add files via upload

---
+++ AMD.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.uts;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class AMD extends cpu {
+    public AMD(int kecepatan) {
+        super("AMD", 3);
+    }
+}
